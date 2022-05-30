649fe34e118222e0a7dda7e677c8245a56baef70
Dom-Code <59179076+Dom-Code@users.noreply.github.com>
2022-05-30 16:27:15 -0700

    Deployment

--- public/lib/db-query.js
+++ public/lib/db-query.js
@@ -1,8 +1,16 @@
 const { Client } = require('pg');
+const config = require('./config');
+
+const isProduction = (config.NODE_env === 'production');
+const CONNECTION = {
+  connectionString: config.DATABASE_URL,
+  ssl: isProduction,
+};
 
 module.exports = {
   async dbQuery(statement, values) {
-    const client = new Client({ database: 'todo-list' });
+    // const client = new Client({ database: 'todo-list' });
+    const client = new Client(CONNECTION);
     await client.connect();
     try {
       const result = await client.query(statement, values);
